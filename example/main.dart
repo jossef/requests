6b8122e2b46b07544686efdc474e146a0f65371e
sehnryr <sehnryr@gmail.com>
2022-03-23 21:00:09 +0100

    feat(exemple): add small exemple

---
+++ example/main.dart
@@ -0,0 +1,17 @@
+import 'package:requests/requests.dart';
+
+void main(List<String> arguments) async {
+  // This example uses the Google Books API to search for books about requests.
+  // https://developers.google.com/books/docs/overview
+  var url = 'https://www.googleapis.com/books/v1/volumes?q={requests}';
+
+  // Await the http get response, then decode the json-formatted response.
+  var response = await Requests.get(url);
+  if (response.statusCode == 200) {
+    var jsonResponse = response.json();
+    var itemCount = jsonResponse["totalItems"];
+    print('Number of books about requests: $itemCount.');
+  } else {
+    print('Request failed with status: ${response.statusCode}');
+  }
+}
