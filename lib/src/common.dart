1712b168a8c61654e4dbc94656066d70f36cb7ca
Gabriel Tavernini <taverninigabriel@gmail.com>
2022-04-24 15:39:02 +0200

    fix: Updated cookie parsing algorithm (#47)

--- lib/src/common.dart
+++ lib/src/common.dart
@@ -89,26 +89,4 @@ class Common {
       return '$k=$v';
     }).join('&');
   }
-
-  static List<String> split(String string, String separator, {int max = 0}) {
-    var result = <String>[];
-
-    if (separator.isEmpty) {
-      result.add(string);
-      return result;
-    }
-
-    while (true) {
-      var index = string.indexOf(separator, 0);
-      if (index == -1 || (max > 0 && result.length >= max)) {
-        result.add(string);
-        break;
-      }
-
-      result.add(string.substring(0, index));
-      string = string.substring(index + separator.length);
-    }
-
-    return result;
-  }
 }
