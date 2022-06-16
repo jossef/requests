0f097d2c544348f31013c95037175a2d99aa27da
MarcoDiGioia <marco.digioia.dev@gmail.com>
2022-06-16 23:39:28 +0200

    revert buggy web support d7f9e231bc0c8df212a0dc10f782bf03374d3613 (#73)
    Co-authored-by: Marco Di Gioia <marcodigioia@Macbook-Pro-Marco.local>

--- lib/src/requests.dart
+++ lib/src/requests.dart
@@ -1,8 +1,8 @@
 import 'dart:core';
-import 'dart:io' if (dart.library.html) '';
+import 'dart:io';
 
 import 'package:http/http.dart';
-import 'package:http/io_client.dart' if (dart.library.html) '';
+import 'package:http/io_client.dart';
 
 import 'package:requests/src/common.dart';
 import 'package:requests/src/cookie.dart';
